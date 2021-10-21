d350311083b79ea667aa6d96837b602a89d725b2
srmrox <shahrukhm@hsco.pk>
2021-10-22 00:21:57 +0300

    cryptocurrency works

--- index.js
+++ index.js
@@ -1,36 +1,81 @@
 const { createHash } = require('crypto');
+const h2b = require('hex-to-binary');
+
+class Transaction {
+    constructor(from, to, amount) {
+        this.from = from;
+        this.to = to;
+        this.amount = amount;
+    }
+}
 
 class Block {
-    constructor(index, timestamp, data, previousHash = '') {
-        this.index = index;
-        this.timestamp = timestamp;
-        this.data = data;
-        this. previousHash = previousHash;
+    constructor(transactions, previousHash='') {
+        this.timestamp = Date.now();
+        this.transactions = transactions;
+        this.previousHash = previousHash;
         this.hash = this.calculateHash();
+        this.nonce = 0;
     }
 
     calculateHash() {
-        return createHash('sha256').update(this.index + this.timestamp + JSON.stringify(this.data) + this.previousHash).digest('hex');
+        return createHash('sha256').update(this.timestamp + JSON.stringify(this.transactions) + this.previousHash + this.nonce).digest('hex');
+    }
+
+    mine(difficulty){
+        while(h2b(this.hash).toString().substring(0,difficulty) !== Array(difficulty + 1).join("0")){
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
     }
 }
 
 class Blockchain {
     constructor() {
         this.chain = [this.createGenesisBlock()];
+        this.difficulty = 2;
+        this.transactionPool = [];
+        this.reward = 100;
     }
 
     createGenesisBlock() {
-        return new Block(0,'1/10/21','no one saves you','0');
+        return new Block('no one saves you','0');
     }
 
     getLatestBlock() {
         return this.chain[this.chain.length - 1];
     }
 
-    addBlock(newBlock) {
-        newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
-        this.chain.push(newBlock);
+    mineTransactionPool(rewardAddress){
+        let block = new Block(this.transactionPool, this.getLatestBlock().hash);    // this list passed here determines which transactions are included in a block
+        block.mine(this.difficulty);
+        this.chain.push(block);
+
+        this.transactionPool = [
+            new Transaction(null, rewardAddress, this.reward)   // having a from address here means mining reward is paid by someone but it's possible they don't have sufficient money. since there are too many transactions that are being mined and the received would want to recieve the full amount, can't dedcut from transactions
+        ];
+    }
+
+    createTransaction(from, to, amount){
+        this.transactionPool.push(new Transaction(from, to, amount));
+    }
+
+    getBalance(address){
+        let balance = 0;
+
+        for (const block of this.chain) {
+            for (const transaction of block.transactions){
+                if(transaction.from === address){
+                    balance -= transaction.amount;
+                }
+
+                if(transaction.to === address){
+                    balance += transaction.amount;
+                }
+            }
+        }
+
+        return balance;
     }
 
     isValid() {
@@ -48,9 +93,11 @@ class Blockchain {
 
 let Chain0One = new Blockchain();
 
-
-Chain0One.addBlock(new Block(1,'1/10/21',{amount: 1}));
-Chain0One.addBlock(new Block(2,'1/10/21',{amount: 2}));
+Chain0One.createTransaction('hina','shahrukh',100);
+Chain0One.createTransaction('shahrukh','malik',50);
+Chain0One.mineTransactionPool('shahrukh');
+Chain0One.mineTransactionPool('shahrukh');
 
 console.log(JSON.stringify(Chain0One, null, 4));
 console.log(Chain0One.isValid());
+console.log(Chain0One.getBalance('shahrukh'));
